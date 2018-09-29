ccc3ba7f507bb9723be5209897d3dfce3f389673
Ryne Kitzrow <rynekitzrow@gmail.com>
2018-09-29 14:51:49 -0400

    testing form input

--- SimplePost.py
+++ SimplePost.py
@@ -1,19 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def my_form():
-    return render_template('my-form.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
-
-
-if __name__ == "__main__":
-    app.run(port=8005)
 
